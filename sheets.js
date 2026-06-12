ff826f8cf07ed45c0e123d8096370952ea94a828
Ikenna Ene <enebret@yahoo.com>
2026-06-12 12:22:19 +0100

    update 003

--- sheets.js
+++ sheets.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { google } = require('googleapis');
 const path = require('path');
 
@@ -17,7 +18,20 @@ async function extractSheetData() {
   try {
     // Authenticate using the service account key file
     const auth = new google.auth.GoogleAuth({
-      keyFile: KEYFILEPATH,
+      credentials: {
+        type: process.env.GOOGLE_TYPE,
+        project_id: process.env.GOOGLE_PROJECT_ID,
+        private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+        // The regex replaces literal '\n' text strings with actual newline formatting
+        private_key: process.env.GOOGLE_PRIVATE_KEY ? process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        auth_uri: process.env.GOOGLE_AUTH_URI,
+        token_uri: process.env.GOOGLE_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+        universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN
+      },
       scopes: SCOPES,
     });
 
